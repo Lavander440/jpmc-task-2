f23222d782a066d47e82d58cb4bcf2ffaf31a964
Chris Lavan <clavan440@gmail.com>
2023-01-18 14:21:18 -0500

    jpmc-task-2 finished

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ class App extends Component<{}, IState> {
       this.setState({
        data: serverResponds,
         showGraph: true,
-       });
+      });
       });
     x++;
     if (x > 1000) {
